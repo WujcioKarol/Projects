1d5ab6795320519e342279639ee4019601436719
WujcioKarol <karislomka@gmail.com>
2022-07-28 21:43:29 +0200

    ToDoList chyba dzialaale nadal nie chce sie zabic

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,48 +1,79 @@
-const editBtns = document.querySelectorAll("[data-edit]");
-const deleteBtns = document.querySelectorAll("[data-delete]");
-const editBtnsContent = document.querySelectorAll("[data-edit-content]");
-const confirmBtns = document.querySelectorAll("[data-confirm]");
-const optionsDiv = document.querySelectorAll("[data-options]");
-const contentDiv = document.querySelectorAll("[data-content]");
-elementDivs = document.querySelectorAll("[data-element]");
-priorityDivs = document.querySelectorAll("[data-priority]");
-priorityDivs.forEach((priorityDiv) => {
-  divcount = priorityDiv.childElementCount;
-  editId = priorityDiv.id.slice(-1) - 1;
-  if (divcount === 3) {
-    contentDiv.item(editId).classList.add("high");
-  } else if (divcount === 2) {
-    contentDiv.item(editId).classList.add("medium");
-  } else if (divcount === 1) {
-    contentDiv.item(editId).classList.add("low");
+const taskList = document.querySelector(".tasks-list");
+const inputTask = document.querySelector("#input_task");
+const addButton = document.querySelector(".fa-plus");
+let TaskElements = document.querySelectorAll(".task-elem");
+let delBtn = document.querySelectorAll(".fa-trash");
+let doneBtn = document.querySelectorAll(".fa-clipboard-check");
+function task(name) {
+  return `
+    <div class='task-name'>
+        <p>${name}</p>
+    </div>
+    <div class="task-buttons">
+        <a class='icon'><i class="fa fa-clipboard-check"></i></a>
+        <a><i class="fa fa-trash icon" aria-hidden="true"></i></a>
+    </div>
+`;
+}
+var Tasks = localStorage.getItem("Tasks");
+var Tasks = Tasks ? JSON.parse(Tasks) : {};
+downloadTasks();
+updateTasks();
+inputTask.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    addTask(inputTask.value, false);
+    updateTasks();
   }
-  console.log(priorityDiv.childElementCount);
 });
-editBtns.forEach((element) => {
-  element.addEventListener("click", () => {
-    editId = element.id.slice(-1) - 1;
+function addTask(name, state) {
+  Object.assign(Tasks, { [name]: state });
+  localStorage.clear();
+  localStorage.setItem("Tasks", JSON.stringify(Tasks));
+  delBtn = document.querySelectorAll(".fa-trash");
+}
+function downloadTasks() {
+  return JSON.parse(localStorage.getItem("Tasks"));
+}
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
 
-    console.log(elementDivs.item(editId).offsetHeight);
-    optionsDiv.item(editId).style.opacity = 1;
-    optionsDiv.item(editId).style.visibility = "visible";
-    contentDiv.item(editId).style.opacity = 0;
-    contentDiv.item(editId).style.visibility = "hidden";
+function updateTasks() {
+  removeAllChildNodes(taskList);
+  Object.entries(Tasks).map((taskElem) => {
+    var taskdiv = document.createElement("div");
+    taskdiv.setAttribute("class", "task-elem");
+    taskdiv.innerHTML = task(taskElem[0]);
+    if (taskElem[1] === true) {
+      taskdiv.classList.add("done");
+    }
+    taskList.appendChild(taskdiv);
   });
-});
-confirmBtns.forEach((element) => {
-  element.addEventListener("click", () => {
-    editId = element.id.slice(-1) - 1;
-    console.log(editId);
+  delBtn = document.querySelectorAll(".fa-trash");
+  delBtn.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      key = e.target.parentElement.parentElement.parentElement.innerText;
+      key = key.replace(/[\r\n]/gm, "");
+      key = key.trim();
+      e.target.parentElement.parentElement.parentElement.remove();
+      localStorage.setItem("Tasks", JSON.stringify(Tasks));
+    });
+  });
+  doneBtn = document.querySelectorAll(".fa-clipboard-check");
+  doneBtn.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      e.target.parentElement.parentElement.parentElement.classList.toggle(
+        "done"
+      );
+      key = e.target.parentElement.parentElement.parentElement.innerText;
+      key = key.replace(/[\r\n]/gm, "");
+      key = key.trim();
 
-    contentDiv.item(editId).style.opacity = 1;
-    contentDiv.item(editId).style.visibility = "visible";
-    optionsDiv.item(editId).style.opacity = 0;
-    optionsDiv.item(editId).style.visibility = "hidden";
+      Tasks[key] = Tasks[key] == true ? false : true;
+      localStorage.clear();
+      localStorage.setItem("Tasks", JSON.stringify(Tasks));
+    });
   });
-});
-deleteBtns.forEach((element) => {
-  element.addEventListener("click", () => {
-    editId = element.id.slice(-1) - 1;
-    elementDivs.item(editId).remove();
-  });
-});
+}
